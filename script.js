237c855bbbc709f728190a4e617c57bc8d4aa272
Jitendra Sharma <jitendravishvakarma708097@gmail.com>
2025-03-04 06:23:46 +0530

    second commit

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const detail = fetch(" https://geeks-for-geeks-api.vercel.app/jitendrasha6vrm");
-// console.log(detail);
-detail.then((data)=>{
-      const body = data.json();
-      body.then((response)=>{
-        console.log(response);
-      })
+const user_name0 = document.getElementById('username');
+const user_name = user_name0.value;
+
+const button = document.getElementById('button');
+
+button.addEventListener('click',()=>{
+  const detail = fetch(`https://geeks-for-geeks-api.vercel.app/${user_name}`);
+  // console.log(detail);
+  detail.then((data)=>{
+        const body = data.json();
+        body.then((response)=>{
+          // console.log(response);
+
+          const profile = document.getElementById('profile-info');
+          profile.innerHTML = response.info.userName;
+        })
+  })
+  .catch((error)=>{
+      console.log(error)
+  });
 })
-.catch((error)=>{
-    console.log(error)
-});
+
